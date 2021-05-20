642c04188682b3e5987a6f90287d95d6b202befa
Quentin Pautrat <quentin.pautrat@octo.com>
2021-05-20 18:37:08 +0200

    init test.

---
+++ src/test/FooTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FooTest {
+
+    @Test
+    public void fooTest() {
+        Assertions.assertTrue(false);
+    }
+}
